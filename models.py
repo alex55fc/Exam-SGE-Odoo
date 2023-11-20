f49c2956139aae986a77785891f89371b30d9f97
alexa <alexander.55.fc@gmail.com>
2023-11-20 08:48:45 +0100

    Segunda clase casi completa, cerveza

--- models.py
+++ models.py
@@ -1,5 +1,20 @@
 from odoo import api, models, fields
 #clases
+class cerveza(models.Model):
+    _name = 'cerveza'
+    name = fields.Char(required=True)
+    tipo = fields.Char()
+    descripcion = fields.Text()
+    contenidoalcohol = fields.Float()
+    preciounidad = fields.Float()
+    volumenunidad = fields.Float()
+    """disponible = fields.Boolean(string='Disponible', compute='_compute_disponible')
+    Disponible -> fields.Boolean, calculado automáticamente en función del inventario
+    Realiza un filtro de búsqueda para conocer las cervezas agotadas
+    Realiza un filtro de búsqueda para conocer las cervezas disponibles
+    Debes permitir buscar por Tipo, Contenido de alcohol, Volumen por unidad, Precio por unidad
+
+    """
 class distribuidor(models.Model):
     _name = 'distribuidor'
     name = fields.Char(required=True)
